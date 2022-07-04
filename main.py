d8f0ce27f6db3121270193ffffcbb1c034f2f089
RIBYHEAR <108630241+RIBYHEAR@users.noreply.github.com>
2022-07-05 03:19:38 +1000

    🍌First Commit!

--- main.py
+++ main.py
@@ -1,53 +1,49 @@
-import os
+import os, colorama
+from colorama import Fore, init
+init(autoreset=True)
 os.system('title Choices - Live or Die')
-print("Choices - Live or Die"
-name=input("What is your name? ")
-age=int(input("What is your age? "))
+print(f"{Fore.GREEN}Hello, Welcome to Choices - Live or Die!")
+name=input(f"{Fore.GREEN}What is your name? ")
+age=int(input(f"{Fore.GREEN}What is your age? "))
 
-if age >=18:
-    print(f"Hello {name}, you are old enough to play!")
-    wants_to_play= input("Do you want to play? ").upper()
+if age >=13:
+    print(f"{Fore.GREEN}[AGE VERIFICATION]{Fore.RESET}Hello {name}, you are old enough to play!")
+    wants_to_play= input(f"{Fore.GREEN}[GAME]{Fore.RESET}Do you want to play? ").upper()
     if wants_to_play == "YES":
-        print("Great, lets play!")
+        print(f"{Fore.GREEN}Great, lets play!")
         health = 100
-        print(f"You are starting with {health} health.")
-        left_or_right= input("First Question... Do you want to go left or right? ").lower()
+        print(f"{Fore.GREEN}[HEALTH]{Fore.RESET}You are starting with {health} health.")
+        left_or_right= input(f"{Fore.GREEN}First Question... Do you want to go left or right? ").lower()
         if left_or_right == "left":
-            ans = input("Good job you followed the path safely to your location you have reached a river do you swim acroos or take the boat?(swim/boat) ").lower() 
+            ans = input(f"{Fore.GREEN}[GAME]{Fore.RESET}Good job you followed the path safely to your location you have reached a river do you swim acroos or take the boat?(swim/boat) ").lower() 
             if ans=='boat':
-                print("you got across the river safely and have now reached the other side.")
+                print(f"{Fore.GREEN}[GAME]{Fore.RESET}you got across the river safely and have now reached the other side.")
 
             elif ans == 'swim':
-                print("you managed to get across the river but got bitten by a crocodile and lost 50 health.")
+                print(f"{Fore.RED}[GAME]{Fore.RESET}you managed to get across the river but got bitten by a crocodile and lost 50 health.")
                 health -= 50
             else:
-                print('Sorry, that is not a valid input. Please restart the program.')
+                print(f'{Fore.RED}Sorry, that is not a valid input. Please restart the program.')
 
-            ans = input("You see a cottage and a bakery. which do you go to?(cottage/bakery) ")
+            ans = input(f"{Fore.GREEN}[QUESTION]{Fore.RESET}You see a cottage and a bakery. which do you go to?(cottage/bakery) ")
             if ans == "cottage":
-                print("You go to the cottage and are greeted by the owner...they don't like you very much you lose 50 health.")
+                print(f"{Fore.RED}[GAME]{Fore.RESET}You go to the cottage and are greeted by the owner...they don't like you very much you lose 50 health.")
                 health-= 50
                 if health==0:
-                    print("You have zero health you have now lost the game is over...")
+                    print(f"{Fore.RED}You have zero health you have now lost the game is over...")
                 else:
-                    print("You have survived for now...")
+                    print(f"{Fore.RED}[GAME]{Fore.RESET}You have survived for now...")
             elif ans == "bakery":
-                print("You go to the bakery, then you stumble uppon some croisants. You eat them and die due to poisoning.")
-                print('Game Over...')
+                print(f"{Fore.RED}[GAME]{Fore.RESET}You go to the bakery, then you stumble uppon some croisants. You eat them and die due to poisoning.")
+                print(f'{Fore.RED}Game Over...')
             else:
-                print('Sorry, that is not a valid input. Please restart the program.')
+                print(f'{Fore.RED}Sorry, that is not a valid input. Please restart the program.')
         elif left_or_right == "right":
-            print('You fall into a hole and die...')
-            print('Game Over...')
+            print(f'{Fore.RED}[GAME]{Fore.RESET}You fell into a hole and died...')
+            print(f'{Fore.RED}[GAME]{Fore.RESET}Game Over...')
         else:
-            print('Sorry, that is not a valid input. Please restart the program.')
+            print(f'{Fore.RED}Sorry, that is not a valid input. Please restart the program.')
     elif wants_to_play == "NO":
-        print("Sorry to see you go...")
+        print(f"{Fore.RED}Sorry to see you go...")
 else:
-    print(f"Sorry {name}, you are not old enough to play.")
-        
-
-       
-
-   
-
+    print(f"{Fore.RED}Sorry {name}, you are not old enough to play.")
